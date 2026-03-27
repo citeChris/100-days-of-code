6fe702b9a14de3aaff6129e2aeedb100ba3e1333
citeChris <citechris.ng@gmail.com>
2026-03-27 20:52:40 +0000

    git commit -m "Add JavaScript projects: Trivia Bot and Sentence Maker"

---
+++ build-a-javascript-trivia-bot.js
@@ -0,0 +1,19 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+const botName = "CodeBuddy";
+const botLocation = "the Cloud";
+const favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " was created in just 10 days by Brendan Eich in 1995 for Netscape to meet the browser markets demands.";
+console.log(codingFact);
+
+codingFact = "Despite its name, " + favoriteLanguage + " is entirely different from Java. While Java is statically typed and object-oriented, ${favoriteLanguage} is dynamically typed and supports both functional and object-oriented programming.";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is the foundation of modern web development, working alongside HTML and CSS to create dynamic and interactive web applications.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
---
+++ build-a-sentence-maker.js
@@ -0,0 +1,19 @@
+let adjective = "brave";
+let noun = "dragon";
+let verb = "dancing";
+let place = "castle";
+let adjective2 = "fiery";
+let noun2 = "pizzas";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory);
+
+adjective = "tiny";
+noun = "unicorn";
+verb = "singing";
+place = "forest";
+adjective2 = "sparkly";
+noun2 = "cupcakes";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: " + secondStory);
