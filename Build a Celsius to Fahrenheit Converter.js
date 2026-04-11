567708aadd5d6131cbd8c3e15d7b430ffa8f3ac9
citeChris <citechris.ng@gmail.com>
2026-04-11 19:40:04 +0100

    git commit -m "Add a Celsius to Fahrenheit Converter"

---
+++ Build a Celsius to Fahrenheit Converter.js
@@ -0,0 +1,7 @@
+function convertCtoF(celsius) {
+  return celsius * (9/5) + 32;
+}
+
+console.log(convertCtoF(0));   // 32
+console.log(convertCtoF(-30)); // -22
+console.log(convertCtoF(20));  // 68
